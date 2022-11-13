7b68fb41345857d8a9459044569ddd185ec9541e
hari <hari.deepak1@gmail.com>
2022-11-13 04:11:46 -0600

    Added missing file

--- backend/backend/admin.py
+++ backend/backend/admin.py
@@ -1,6 +1,6 @@
 import django.contrib.admin as admin
-from backend.models import Profession, UserInfo, Emergency
+from backend.models import Profession, UserData, Emergency
 
 admin.site.register(Profession)
-admin.site.register(UserInfo)
+admin.site.register(UserData)
 admin.site.register(Emergency)
