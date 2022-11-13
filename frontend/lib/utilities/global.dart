6e67cf3f226c9894e6cd4ee30325223d7813a305
Rainam98 <rainams98@gmail.com>
2022-11-12 23:20:09 -0600

    Added Login page and called integrated login api.

--- frontend/lib/utilities/global.dart
+++ frontend/lib/utilities/global.dart
@@ -1,4 +1,5 @@
 class Global {
-  static String djangoUrl = "http://localhost:8000";
+  static String djangoUrl = "http://127.0.0.1:8000";
   static String homePage = "$djangoUrl/home";
+  static String loginPage = "$djangoUrl/login";
 }
